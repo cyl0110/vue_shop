18ce0a9d0102cf3e4450e4202f55611049a917de
曹玉龙 <731057508@qq.com>
2021-12-07 22:30:59 +0800

    完成了商品功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '@/components/power/rights'
 import Roles from '@/components/power/Roles'
 import Cate from '@/components/goods/Cate'
 import Params from '@/components/goods/Params'
+import List from '@/components/goods/List'
+import Add from '@/components/goods/Add'
 
 Vue.use(Router)
 
@@ -25,7 +27,9 @@ const router = new Router({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: List },
+        { path: '/goods/add', component: Add }
       ]
     }
   ]
